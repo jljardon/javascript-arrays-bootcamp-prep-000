cc1d043916346a105ef63f5001821c4c68a5bdc8
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-03 02:32:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,8 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 }
 
 function addElementToEndOfArray(array, element){
-
+  array=[...array,element]
+  return array
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
