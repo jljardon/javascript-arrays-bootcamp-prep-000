b4a9286ffa2d61dba0db9b6b60c91be77d92fe7e
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-03 04:19:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,6 +35,6 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  array.slice(0, array.length - 1)
+  var array = array.slice(0, array.length - 1)
   return array
 }
